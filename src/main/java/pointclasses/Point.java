59a5de209f1b98bd4abb5f869b79cb4509b1a90d
Timirbulatov <aidar126621@gmail.com>
2020-09-22 12:56:32 +0400

    выполнено задание 1.3

---
+++ src/main/java/pointclasses/Point.java
@@ -0,0 +1,16 @@
+package pointclasses;
+
+public class Point {
+    public final double X;
+    public final double Y;
+    public final double Z;
+
+    public Point(double X, double Y, double Z){
+        this.X=X;
+        this.Y=Y;
+        this.Z=Z;
+    }
+    Point point1 = new Point(0,0,0);
+    Point point2 = new Point(1, 1, 1);
+    Point point3 = new Point(1, 2, 3);
+}
