a35388be392de5d08651aba224a92190b30fd70c
Timirbulatov <aidar126621@gmail.com>
2021-01-13 22:27:30 +0400

    выполнено задание 3.23

---
+++ src/main/java/ru/ssau/tk/stringmetods/Task3_23.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.stringmetods;
+
+import java.nio.charset.Charset;
+
+public class Task3_23 {
+    public static void main(String[] args) {
+        System.out.println(Charset.defaultCharset());
+    }
+}
