ee7355221dd37973d1d519ae5c0f6d5f2436cec8
Timirbulatov <aidar126621@gmail.com>
2021-01-13 14:51:07 +0400

    выполнено задание 3.6 \t  — Символ табуляции (в java – эквивалент четырех пробелов); \b — Символ возврата в тексте на один шаг назад или удаление одного символа в строке (backspace); \n — Символ перехода на новую строку; \r — Символ возврата каретки; \f— Прогон страницы к началу следующей страницы; \’ — Символ одинарной кавычки; \” — Символ двойной кавычки; \\ — Символ обратной косой черты (\).

---
+++ src/main/java/ru/ssau/tk/stringmetods/Task3_6.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.stringmetods;
+
+public class Task3_6 {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println("Символ \t№" + ++i);
+        System.out.println("Символ \b№" + ++i);
+        System.out.println("Символ \n№" + ++i);
+        System.out.println("Символ \r№" + ++i);
+        System.out.println("Символ \'№" + ++i);
+        System.out.println("Символ \"№" + ++i);
+        System.out.println("Символ\\№" + ++i);
+    }
+}
