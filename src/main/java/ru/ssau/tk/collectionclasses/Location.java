d76114a766e25c11752df8497f281c1468627fed
Timirbulatov <aidar126621@gmail.com>
2021-01-14 21:54:44 +0400

    выполнено задание 5.4

--- src/main/java/ru/ssau/tk/collectionclasses/Location.java
+++ src/main/java/ru/ssau/tk/collectionclasses/Location.java
@@ -1,5 +1,7 @@
 package ru.ssau.tk.collectionclasses;
 
+import java.util.Objects;
+
 public class Location {
     private int id;
     private String name;
@@ -40,5 +42,18 @@ public class Location {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Location location = (Location) object;
+        return id == location.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
