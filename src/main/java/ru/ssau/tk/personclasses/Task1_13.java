57f1cbdf050dd74cb1d6741668493a76e0d94939
Timirbulatov <aidar126621@gmail.com>
2021-01-15 12:38:43 +0400

    выполнено задание 1.13 Сначала выполняется метод checkInt() и выводится число 10, несмотря на то, что передается 5. Это происходит из-за того, что аргументы-примитивы передаются по значению и их можно редактировать. Аналогично с методом checkPerson — только Person это ссылочный тип, в аргументе передается ссылка на объект person. Внутри main выдаст 5, т.к. копия с number = 10 осталась в методе checkInt, а имя выведет Oleg, т.к. в метод checkPerson объект person передавался по ссылке и имя изменилось у объекта вообще, созданного в main-классе

---
+++ src/main/java/ru/ssau/tk/personclasses/Task1_13.java
@@ -0,0 +1,24 @@
+package ru.ssau.tk.personclasses;
+
+public class Task1_13 {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setFirstName("Arkadiy");
+        int number = 5;
+        checkInt(number);
+        checkPerson(person);
+        System.out.println(number);
+        System.out.println(person.getFirstName());
+    }
+
+    private static void checkInt(int number) {
+        number = 10;
+        System.out.println(number);
+    }
+
+    private static void checkPerson(Person person) {
+        person.setFirstName("Oleg");
+        System.out.println(person.getFirstName());
+    }
+
+}
